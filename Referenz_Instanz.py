83821f51cf1556a816624245880352e63cbd0a98
fuchs <60089303+cyber-fuchs@users.noreply.github.com>
2020-10-01 13:44:10 +0200

    Kapitel 7

---
+++ Referenz_Instanz.py
@@ -0,0 +1,11 @@
+willkommen="Herzlich Willkommen!"
+print(willkommen)
+print("ID von willkommen: ",id(willkommen))
+del(willkommen)
+#print("ID von willkommen: ",id(willkommen))
+
+a=1
+b=1
+print("a=1 id:",id(a))
+print("b=1 id:",id(b))
+print("a is b:",a is b)
---
+++ mutable_vs_immutable.py
@@ -0,0 +1,17 @@
+a="Wasser"
+a+="flasche"
+print(a)
+
+a=[1,2]
+a+=[3,4]
+print(a)
+
+a="Wasser"
+b=a
+a+="flasche"
+print(b,"\n",a)
+
+a=[1,2]
+b=a
+a+=[3,4]
+print(b,"\n",a)
